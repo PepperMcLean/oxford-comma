969810257a124151fad950146a54ff021024fa8c
PepperMcLean <jackmcl206@gmail.com>
2021-02-10 12:00:14 -0600

    commit tree 04d2a83c7741b8719930ee42e85396a3a6e63cc0

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length() > 2
+        newArray = []
+        newArray = array[0, (array.length-1)]
+        x = newArray.join(", ")
+        return x + ", and #{array[-1]}"
+    elsif array.length() == 2
+        return array.join(" and ")
+    else 
+        return array.join()
+    end
 end
